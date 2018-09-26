219d37698d967f38a2fc633a3dfc4c7eefced0d3
Hassan Malik <hbmalik88@gmail.com>
2018-09-26 19:39:53 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,7 @@
 def introduction(name)
   put "Hi, my name is #{name}.\n"
 end
+
+def introduction_with_language(name, language)
+  put "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+end
